77f28b453b71f0a2ba2cb900ec932ec076837a9d
Sarthak Verma <hisarthakverma@gmail.com>
2020-03-02 18:05:42 +0530

    Changed Circle Services

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@ $subject = $_POST["subject"];
 $message = $_POST["message"];
 
 
-$EmailTo = "000.lunchbox@gmail.com";
+$EmailTo = "hisarthakverma@gmail.com";
 $Title = "New Message Received";
 
 // prepare email body text
